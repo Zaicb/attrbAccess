ba8670a13b0a39d380471afb0d26bc3ca0d22353
Zaicb <logic.self@gmail.com>
2014-09-30 20:49:56 +0300

    1

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 import datetime
 import time
 
-PathFile = os.stat('D:\\pagefile.sys')
+PathFile = os.stat('C:\\boot.ini')
 st_time = PathFile.st_mtime
 a, b, c, d, e, f, g, h, i=time.localtime(st_time)
 print datetime.datetime(a, b, c, d, e, f, g)
@@ -25,7 +25,7 @@ import win32api
 print 'attribs'
 atrbs = win32api.GetFileAttributes('C:\\boot.ini')
 print atrbs
-win32api.SetFileAttributes('C:\\Users\\bugZ_Z\\Downloads\\pywin32-219.zip',
+win32api.SetFileAttributes('C:\\boot.ini',
                            win32con.FILE_ATTRIBUTE_NORMAL
                            )
 print atrbs
